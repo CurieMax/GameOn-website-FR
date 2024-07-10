e4e97ff3704059c6fb3cc5afff7f2f68ebc13576
Max Curie <curie.max@yahoo.com>
2024-07-10 12:05:46 +0200

    :feat: add close modal form

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,9 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
+// Const close btn
+const btnCloseModal = document.querySelectorAll(".close");
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -20,4 +23,11 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal form
+function closeModal(reset) {
+  modalbg.style.display = "none"
+}
 
+btnCloseModal.forEach((closeBtn) => closeBtn.addEventListener("click", () => {
+  closeModal(true)
+}));

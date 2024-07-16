e2e093324e731d9f34e5ca4fc73e11e18ce5f922
Max Curie <curie.max@yahoo.com>
2024-07-16 15:39:43 +0200

    :feat: error message form + reset form

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,15 @@
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const btnCloseModal = document.querySelector(".close");
+const firstNameInput = document.querySelector("#first");
+const lastNameInput = document.querySelector("#last");
+const emailInput = document.querySelector("#email");
+const birthdateInput = document.querySelector("#birthdate");
+const quantityInput = document.querySelector("#quantity");
+
+
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -7,13 +19,7 @@ function editNav() {
   }
 }
 
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
 
-// Const close btn
-const btnCloseModal = document.querySelectorAll(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -28,6 +34,25 @@ function closeModal(reset) {
   modalbg.style.display = "none"
 }
 
-btnCloseModal.forEach((closeBtn) => closeBtn.addEventListener("click", () => {
+function resetForm(){
+  document.getElementById("formulaire").reset();
+}
+
+btnCloseModal.addEventListener("click", () => {
   closeModal(true)
-}));
+  resetForm()
+});
+
+// Validation firstName
+firstNameInput.addEventListener("invalid", (e) => inputErrorMessage(e, "Veuillez entrer 2 caractères ou plus pour le champ du prénom."));
+lastNameInput.addEventListener("invalid", (e) => inputErrorMessage(e, "Veuillez entrer 2 caractères ou plus pour le champ du nom."));
+emailInput.addEventListener("invalid", (e) => inputErrorMessage(e, "Veuillez entrez une adresse email valide."));
+birthdateInput.addEventListener("invalid", (e) => inputErrorMessage(e, "Vous devez entrer votre date de naissance."));
+
+function inputErrorMessage(event, message) {
+  const target = event.target;
+  const parent = target.parentElement;
+
+  parent.setAttribute("data-error", message);
+  parent.setAttribute("data-error-visible", "true");
+}

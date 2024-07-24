0cc8c399599d62dd22671cb2ec00fb973415ea8d
Max Curie <curie.max@yahoo.com>
2024-07-24 18:22:12 +0200

    :feat: add complete validation + confirmation message

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -9,8 +9,8 @@ const emailInput = document.getElementById("email");
 const birthdateInput = document.getElementById("birthdate");
 const quantityInput = document.getElementById("quantity");
 const form = document.getElementById("reserve");
-const checkbox = document.querySelectorAll(".checkbox-input");
-const errorClass  = document.querySelectorAll(".error");
+const errorClass = document.querySelectorAll(".error");
+const confirmationMessage = document.getElementById("confirmationMessage");
 
 function editNav() {
   var x = document.getElementById("myTopnav");
@@ -20,8 +20,6 @@ function editNav() {
     x.className = "topnav";
   }
 }
-
-
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -35,31 +33,34 @@ function launchModal() {
 function closeModal() {
   modalbg.style.display = "none";
   resetForm();
+  form.style.display = "block";
+  confirmationMessage.style.display = "none";
 }
 
-function resetForm(){
+function resetForm() {
   document.querySelector(".form").reset();
 }
 
 btnCloseModal.addEventListener("click", () => {
-  closeModal(true)
-  resetForm()
+  closeModal(true);
+  resetForm();
   errorClass.forEach((ele) => {
     hideErrorMessage(ele);
-  })
+  });
 });
 
 // Submit form
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  
+
   if (validInput()) {
-    form.submit();
+    afterSubmit();
+    return true;
+  } else {
+    return false;
   }
-})
-
-
+});
 
 // show error message
 
@@ -68,7 +69,7 @@ const showErrorMessage = (ele, msg) => {
   const errormsg = inputControl.querySelector(".error");
   errormsg.innerText = msg;
   inputControl.classList.add("errormsg");
-}
+};
 
 // hide error message
 const hideErrorMessage = (ele) => {
@@ -76,8 +77,7 @@ const hideErrorMessage = (ele) => {
   const errormsg = inputControl.querySelector(".error");
   errormsg.innerText = "";
   inputControl.classList.remove("errormsg");
-}
-
+};
 
 // Step validation first name
 
@@ -85,21 +85,21 @@ function checkValidationFirstName() {
   const isvalidFirstName = (firstName) => {
     const reTest = /^[a-zA-Z]+$/;
     return reTest.test(String(firstName).toLowerCase());
+  };
+  const validationFirstName = firstNameInput.value.trim();
+  if (validationFirstName === "") {
+    showErrorMessage(firstNameInput, "Veuillez renseigner votre prénom");
+  } else if (validationFirstName.length < 2) {
+    showErrorMessage(firstNameInput, "Veuillez renseigner un prénom valide");
+  } else if (!isvalidFirstName(validationFirstName)) {
+    showErrorMessage(firstNameInput, "Veuillez renseigner un prénom valide");
+    return false;
+  } else {
+    hideErrorMessage(firstNameInput);
   }
-const validationFirstName = firstNameInput.value.trim();
-if (validationFirstName === "") {
-  showErrorMessage(firstNameInput, "Veuillez renseigner votre prénom");
-} else if (validationFirstName.length < 2) {
-  showErrorMessage(firstNameInput, "Veuillez renseigner un prénom valide");
-} else if (!isvalidFirstName(validationFirstName)) {
-  showErrorMessage(firstNameInput, "Veuillez renseigner un prénom valide");
-} else {
-  hideErrorMessage(firstNameInput);
+
+  return true;
 }
-}
-
-
-
 
 // Step validation last name
 
@@ -107,39 +107,46 @@ function checkValidationLastName() {
   const isvalidLastName = (lastName) => {
     const reTest = /^[a-zA-Z]+$/;
     return reTest.test(String(lastName).toLowerCase());
+  };
+  const validationLastName = lastNameInput.value.trim();
+  if (validationLastName === "") {
+    showErrorMessage(lastNameInput, "Veuillez renseigner votre nom");
+  } else if (validationLastName.length < 2) {
+    showErrorMessage(
+      lastNameInput,
+      "Veuillez entrer 2 caractères ou plus pour le champ du nom"
+    );
+  } else if (!isvalidLastName(validationLastName)) {
+    showErrorMessage(lastNameInput, "Veuillez renseigner un nom valide");
+    return false;
+  } else {
+    hideErrorMessage(lastNameInput);
   }
-const validationLastName = lastNameInput.value.trim();
-if (validationLastName === "") {
-  showErrorMessage(lastNameInput, "Veuillez renseigner votre nom");
-} else if (validationLastName.length < 2) {
-  showErrorMessage(lastNameInput, "Veuillez entrer 2 caractères ou plus pour le champ du nom");
-} else if (!isvalidLastName(validationLastName)) {
-  showErrorMessage(lastNameInput, "Veuillez renseigner un nom valide");
-} else {
-  hideErrorMessage(lastNameInput);
-}
+
+  return true;
 }
 
 // Step Validation Email
 
-
 function checkValidationEmail() {
   const isvalidEmail = (email) => {
-    const reTest = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const reTest =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return reTest.test(String(email).toLowerCase());
-  }
-  
+  };
+
   const validationEmail = emailInput.value.trim();
 
   if (validationEmail === "") {
     showErrorMessage(emailInput, "Veuillez renseigner votre email");
   } else if (!isvalidEmail(validationEmail)) {
-    showErrorMessage(emailInput, "Veuillez entrer un email valide"); 
-  }else {
+    showErrorMessage(emailInput, "Veuillez entrer un email valide");
+    return false;
+  } else {
     hideErrorMessage(emailInput);
   }
 
-
+  return true;
 }
 
 // Step Validation Birthdate
@@ -147,32 +154,78 @@ function checkValidationEmail() {
 function checkValidationBirthdate() {
   const validationBirthdate = birthdateInput.value.trim();
   if (validationBirthdate === "") {
-    showErrorMessage(birthdateInput, "Vous devez entrer votre date de naissance");
+    showErrorMessage(
+      birthdateInput,
+      "Vous devez entrer votre date de naissance"
+    );
+    return false;
   } else {
     hideErrorMessage(birthdateInput);
   }
+
+  return true;
 }
 
+// Step Validation quantity
 
 function checkValidationQuantity() {
   const validationQuantity = quantityInput.value.trim();
   if (validationQuantity === "") {
     showErrorMessage(quantityInput, "Veuillez renseigner un nombre");
+    return false;
   } else {
     hideErrorMessage(quantityInput);
   }
+
+  return true;
 }
 
+// Step Validation checkbox
+
+function checkValidationCheckbox(location) {
+  const radios = document.getElementsByName("location");
+  for (let i = 0; i < radios.length; i++) {
+    if (radios[i].checked) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// step validation conditions
+
+function checkValidationConditions() {
+  const validationCondition = document.getElementsByName("conditions");
+  for (let i = 0; i < validationCondition.length; i++) {
+    if (validationCondition[i].checked) {
+      return true;
+    }
+  }
+  return false;
+}
 
 // validation input
 
 function validInput() {
-  
-checkValidationFirstName();
-checkValidationLastName();
-checkValidationEmail();
-checkValidationBirthdate();
-checkValidationQuantity();
+  if (
+    checkValidationFirstName() &&
+    checkValidationLastName() &&
+    checkValidationEmail() &&
+    checkValidationBirthdate() &&
+    checkValidationQuantity() &&
+    checkValidationCheckbox() &&
+    checkValidationConditions()
+  ) {
+    return true;
+  }
+  return false;
+}
+
+// After submitting the form
+
+function afterSubmit() {
+  form.style.display = "none";
+  confirmationMessage.style.display = "block";
 }
 
 // Keep data in localStorage
@@ -180,6 +233,3 @@ checkValidationQuantity();
 firstNameInput.value = localStorage.getItem("first");
 lastNameInput.value = localStorage.getItem("last");
 emailInput.value = localStorage.getItem("email");
-
-
-
